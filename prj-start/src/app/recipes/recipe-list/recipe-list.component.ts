beb16d3890468235b24bdf61c5f5b3ac880cefba
Mark Mingo <86744865+mmingo848@users.noreply.github.com>
2023-02-28 20:40:26 -0600

    P1

---
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Empanadas", "A mexican dish", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F06%2F15%2Fair-fryer-empanadas-2000.jpg&q=60")
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
